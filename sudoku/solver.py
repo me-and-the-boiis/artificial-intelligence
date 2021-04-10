c75a4b2cf10b240206e3f57f81cc05677f91c5d5
Ngoc Nguyen Minh <alanke01636253662@gmail.com>
2021-04-10 12:53:39 +0700

    init branch

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -7,6 +7,10 @@ class Solver:
         self.nCandidate = nCandidate  # Number of candidates.
         self.nGeneration = nGeneration  # Number of generations.
         self.nMutation = nMutation  # Number of mutations.
+        self.nElites = int(0.05*self.nCandidate)    # Number of Elite candidates
+        self.phi = 0
+        self.sigma = 0
+        self.mutation_rate = 0.06
     
     def initPopulation(self):
         self.population = Population(self.nCandidate, self.sudoku)
