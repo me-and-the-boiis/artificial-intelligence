956d66b89593d777e9b9b885c94a21030a46b77c
misterbo271 <63491016+misterbo271@users.noreply.github.com>
2021-04-10 15:46:45 +0700

    Delete __init__.py

--- astar_pathfiding/__init__.py
+++
@@ -1,5 +0,0 @@
-from .draw_map import Spot, Path, Obstacle, Button
-from .astar import AStar
-from .car import Car
-
-from .draw_map import SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_TITLE, DIV, DT, G_W, G_H
