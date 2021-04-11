0ac3364258666cea416e2a62ea057f69b3cb57f4
ngvuthanhnhan <ngvuthanhnhan@gmail.com>
2021-04-11 17:25:43 +0700

    5/10

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import random
 
 
 class Board:
-    def __init__(self, state):
+    def __init__(self, state, height):
         self.state = state
+        self.height = height
         for x in range(len(init)):
             for y in range(len(init)):
                 if init[x][y] == 0:
@@ -14,11 +15,11 @@ class Board:
 
     def __eq__(self, other):
         # return (self.state == other.state).all() and self.index == other.index
-        return self.state == other.state and self.index == other.index
+        return self.state == other.state
 
 
 def movement(index, new_index, node):
-    new = Board(deepcopy(node.state))
+    new = Board(deepcopy(node.state), node.height + 1)
     new.state[index[0]][index[1]] = node.state[new_index[0]][new_index[1]]
     new.state[new_index[0]][new_index[1]] = node.state[index[0]][index[1]]
     new.index = new_index
@@ -47,13 +48,18 @@ def generate(node):
 
 
 def dfs(root):
-    visited = list([Board(root)])
-    stack = list([Board(root)])
+    visited = list([Board(root, 0)])
+    stack = list([Board(root, 0)])
+    correct_path = []
     while stack:
         node = stack.pop()
+        if node.height > len(correct_path):
+            correct_path.append(node)
+        elif node.height < len(correct_path):
+            correct_path.pop()
         # if (node.state == goal).all():
         if node.state == goal:
-            return stack
+            return correct_path
         # Generate possible paths
         possible_paths = reversed(generate(node))
         for path in possible_paths:
@@ -69,25 +75,28 @@ def randomme(n):
 
 # init = randomme(2)
 # goal = randomme(2)
-# init = [[1, 2, 5],
-#         [3, 4, 0],
-#         [6, 7, 8]]
+init = [[1, 2, 5],
+        [3, 4, 0],
+        [6, 7, 8]]
 # goal = [[0, 1, 2],
 #         [3, 4, 5],
 #         [6, 7, 8]]
-init = [[2, 1],
-        [0, 3]]
-goal = [[1, 0],
-        [2, 3]]
+goal = [[0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]]
+# init = [[2, 1],
+#         [0, 3]]
+# goal = [[1, 0],
+#         [2, 3]]
 # print(init)
 # print(goal)
 
 
 def main():
     solver = dfs(init)
-    for i in range(len(solver) - 1):
-        print(solver[i], end=" -> ")
-    print(solver[-1])
+    print(init, end="")
+    for i in range(len(solver)):
+        print(" -> ", solver[i].state, end="")
 
 
 if __name__ == '__main__':
